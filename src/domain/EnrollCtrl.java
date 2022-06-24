984a252ee840452bce34b9825aac3417662b78ed
nazerpanahi <nazerpanahi@gmail.com>
2022-06-24 18:03:21 +0430

    refactor: do not throw error on partial condition matches

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -2,22 +2,25 @@ package domain;
 
 import domain.exceptions.EnrollmentRulesViolationException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class EnrollCtrl {
     public void enroll(Student s, List<CSE> courses) throws EnrollmentRulesViolationException {
+        List<String> errorMessages = new ArrayList<>();
         for (CSE o : courses) {
             // check if student passed the course
-            if (s.isPassed(o.getCourse())){
-                throw new EnrollmentRulesViolationException(String.format("The student has already passed %s", o.getCourse().getName()));
+            if (s.isPassed(o.getCourse())) {
+                errorMessages.add(String.format("The student has already passed %s", o.getCourse().getName()));
             }
             // check if student passed all prerequisites
             List<Course> prereqs = o.getCourse().getPrerequisites();
             for (Course pre : prereqs) {
                 if (!s.isPassed(pre)) {
-                    throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s", pre.getName(), o.getCourse().getName()));
+                    errorMessages.add(String.format("The student has not passed %s as a prerequisite of %s", pre.getName(), o.getCourse().getName()));
                 }
             }
 
@@ -25,9 +28,9 @@ public class EnrollCtrl {
             // or taken twice at same time
             for (CSE o2 : courses) {
                 if (o.hasTimeConflict(o2))
-                    throw new EnrollmentRulesViolationException(String.format("Two offerings %s and %s have the same exam time", o, o2));
+                    errorMessages.add(String.format("Two offerings %s and %s have the same exam time", o, o2));
                 if (o.hasCommonCourse(o2))
-                    throw new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice", o.getCourse().getName()));
+                    errorMessages.add(String.format("%s is requested to be taken twice", o.getCourse().getName()));
             }
         }
 
@@ -37,7 +40,10 @@ public class EnrollCtrl {
         if ((gpa < 12 && unitsRequested > 14) ||
                 (gpa < 16 && unitsRequested > 16) ||
                 (unitsRequested > 20))
-            throw new EnrollmentRulesViolationException(String.format("Number of units (%d) requested does not match GPA of %f", unitsRequested, gpa));
+            errorMessages.add(String.format("Number of units (%d) requested does not match GPA of %f", unitsRequested, gpa));
+        if (!errorMessages.isEmpty()) {
+            throw new EnrollmentRulesViolationException(String.join("\n", errorMessages));
+        }
         for (CSE o : courses)
             s.takeCourse(o.getCourse(), o.getSection());
     }
